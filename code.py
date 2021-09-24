580cdfb34fec1d93c4b748eea402f5d84fe76280
PossiblePack <possible.pack@gmail.com>
2021-09-25 04:13:49 +0700

    PrintState_Func

--- code.py
+++ code.py
@@ -10,8 +10,13 @@ def splitdata(x):
 def simulateEX(x):
     print('@@@\nstate:\n\t\tpc 0\n\t\tmemory:')
     arr = splitdata(x)
+    numReg = 8
     for i in range(len(arr)):
         print('\t\t\t\tmemory[{0}]={1}'.format(i,arr[i]))
+    print('\t\tregiters:')
+    for i in range(numReg):
+        print('\t\t\t\treg[{0}]{1}'.format(i,' '))
+    print('end state')
     
 
 simulateEX("test.txt")
