26ccdd25680ace97891441a1bfbe33fd3f67f1cf
phird <iamphird@gmail.com>
2021-09-26 02:28:34 +0700

    fix 2's complement

--- Convert.py
+++ Convert.py
@@ -6,24 +6,25 @@ class ConB:
         self.regA = ""
         self.regB = ""
         self.regC = ""
-        
-        
-    def twosComplement(self):
-        x = self.regC
-        n = len(x)
-        k = n-2
-        while(k >= 0):
-            #flip
-            if(x[k] == 1 ):
-                x = list(x)
-                x[k] = '0'
-                x = ''.join(x)
+    
+    def twos_com(self):
+        d = int(self.regC, 2 )
+        #print("decimal is " + str(d))
+        #print("before flip is " + str(bin(d)).replace('0b', ''))
+        flipped = (d ^ 65536) + 1 
+        tcom = str(bin(flipped)).replace('0b', '')
+        signBit = tcom[0]
+        signed = "1"
+        osigned = "0"
+        while(len(tcom) < 32):  # hard-code extended
+            if(signBit == "1"):
+                tcom = signed + tcom
             else:
-                x = list(x)
-                x[k] = '1'
-                x = ''.join(x)
-            k-=1
-        return x 
+                tcom = osigned + tcom
+        self.regC = tcom
+        #flipped = bin(~d)
+        #print("Sign_BIT is " + signBit)
+
        
     def findReg(self):
         x = self.bi
@@ -43,16 +44,15 @@ class ConB:
             self.regA = x[3:6]
             self.regB = x[6:9]
             #self.temp = "temp is invalid"
-            #self.desReg = "destReg is invalid"
-            #self.regC = x[9:25]
-            if(x[9] == "1" ):
-                print("       - use 2's complement -       ")
-                p = ConB.twosComplement(self)
-                self.regC =  p
+            #self.desReg = "destReg is invalid
+            self.regC = x[9:25]
+            if( x[9] == "1" ):
+                print(" - use 2's complement - ")               
+                self.twos_com()
             else:
-                print("       - did not use 2's complement -       ")
+                print(" - did not use 2's complement - ")
                 self.regC = x[9:25]
-
+            
         else : # O-type (halt, noop)
             self.opcode = x[0:3]
             self.temp = x[3:25]
@@ -60,41 +60,14 @@ class ConB:
             #self.regB = "resB is invalid"
             #self.desReg = "destReg is invalid"
             #self.offsetField = "offsetFeild is invalid"
+
             
-            
-
-
-""" 
-    print("======================================================================================")
-    convert2 = ConB(8454151)
-    convert2.findReg()
-    print(" lw        0         1         five")
-    print("Maachine code is " + str(convert2.decnum))
-    print("[Code is   ] " + convert2.bi)
-    print("[Opcode is ] " + convert2.opcode)
-    print("[RegA is   ] " + convert2.regA)
-    print("[RegB is   ] " + convert2.regB)
-    print("[desReg is ] " + convert2.desReg)
-    print("[Temp is   ] " + convert2.temp)
-
-
-    print("======================================================================================")
-
-    convert3 = ConB(655361)
-    convert3.findReg()
-    print(" add     1         2         1")
-    print("Maachine code is " + str(convert3.decnum))
-    print("[Code is   ] " + convert3.bi)
-    print("[Opcode is ] " + convert3.opcode)
-    print("[RegA is   ] " + convert3.regA)
-    print("[RegB is   ] " + convert3.regB)
-    print("[desReg is ] " + convert3.desReg)
-    print("[Temp is   ] " + convert3.temp)
-    print("[offsetFeild is ] " + convert3.offsetField) """
-
-print("======================================================================================")
+""" print("======================================================================================")
 convert4 = ConB(16842749)
+print("Code is " + convert4.bi)
 convert4.findReg()
 l = convert4.regC
-print("[offsetFeild is ] " + l)
+print("[offsetFeild is ] " + l) """
+
+
 
