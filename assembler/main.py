fd9c5f697dcf43112a68eecb65537898b24aacc1
mekkkwiz <59276758+mekkkwiz@users.noreply.github.com>
2021-09-26 17:45:18 +0700

    add 2's com fn

--- assembler/main.py
+++ assembler/main.py
@@ -5,33 +5,48 @@ class AssemblyTranslator:
 
     __machineLang = []                      #all instructions in assembly that translated to machine language   
     __fillValue = []                        #collect all .fill variables and make list of pair [symbolic,values]
-    __assembly = [] 
+    __assembly = []                         #save all instructions in assembly line by line
     __inst = Instruction
 
     #*dummy functions 
 
-    def __printer(self):                    #TODO:
-        open("textToSimulator", "w")
-        return
+    def __printer(self):                    #TODO: write machine language to text file
+        open("textToSimulator.txt", "w")
 
-    def __fillFinding(self):                #TODO: this function should be call first and find .fill in assembly file and return line where .fill is                        
-        for i in self.__assembly:
-            if(i[1]==".fill"):
-              sheet =[i[0],i[2]]              
-              self.__fillValue.append(sheet)
-        return                              #TODO: and then collect all .fill variables and make list of pair [symbolic,values]
+    def __twos_com(self, num):
+        d = int(num, 2)
 
-    def __binToDec(self,binary):            #TODO: convert intput num in binary to decimal number
-        if(type(binary) is str):
-            return int(binary,2)                #TODO: intput string binary
-        else:
-            return print('Type of binary not string')
-
-
+        flipped = (d ^ 65536) + 1 
+        tcom = str(bin(flipped)).replace('0b', '')
+        signBit = tcom[0]
+        signed = "1"
+        osigned = "0"
+        while(len(tcom) < 32):  # hard-code extended
+            if(signBit == "1"):
+                tcom = signed + tcom
+            else:
+                tcom = osigned + tcom
+        num = tcom
+        #flipped = bin(~d)
+        #print("Sign_BIT is " + signBit)
 
 
     
     #*used functions
+
+    def __fillFinding(self):                #TODO: this function should be call first and find .fill in assembly file and return line where .fill is                        
+        for i in self.__assembly:           #TODO: and then collect all .fill variables and make list of pair [symbolic,values]
+            if(i[1]==".fill"):
+                sheet =[i[0],i[2]]              
+                self.__fillValue.append(sheet)
+
+
+    def __binToDec(self,binary):            #TODO: convert intput num in binary to decimal number
+        if(type(binary) is str):
+            return int(binary,2)                
+        else:
+            return print('Type of binary not string')
+
 
     def translator(self,item):              #TODO: make this function that
 
@@ -56,7 +71,17 @@ class AssemblyTranslator:
             textTranslated += optc_bin
             textTranslated += self.__regDecoder(regA)
             textTranslated += self.__regDecoder(regB)
-            textTranslated += "2’s complement(num)"
+
+            destReg_symbolic = ""
+            isSymbolic = False
+            for i in range(len(self.__fillValue)):
+                if(self.__fillValue[i][0] == destReg):
+                    destReg_symbolic = self.__fillValue[i][1]
+                    isSymbolic = True
+                    break;
+                isSymbolic = False
+
+            textTranslated += self.__twos_com(int(destReg_symbolic)) if isSymbolic else self.__twos_com(destReg)
 
         elif type == "J" :
             textTranslated += "0000000"
@@ -77,7 +102,6 @@ class AssemblyTranslator:
 
         print(textTranslated)        #!for debugging purposes
         
-        return
 
     def __regDecoder(self,number):                  #TODO: decode reg from dec to bin like from '5' to '101'
         number = int(number)     
@@ -89,7 +113,7 @@ class AssemblyTranslator:
             elif(number<4):
                 return"0"+bin(number).replace("0b", "")
             elif(number>3):
-                return bin(number).replace("0b", "")   #? https://www.geeksforgeeks.org/python-program-to-covert-decimal-to-binary-number/
+                return bin(number).replace("0b", "")        #? https://www.geeksforgeeks.org/python-program-to-covert-decimal-to-binary-number/
         # elif number < 0 and number > -7:
         #    return bin(number if number>0 else number+(1<<3)).replace("0b", "")
         else: 
@@ -102,10 +126,10 @@ class AssemblyTranslator:
 
         for item in listTransformed:
             if (item[0] in Instruction["name"]):    #ckeck instions of this line have the same name as the instruction in the instruction list
-                if (item[0]  == "halt"):
-                    labels, instcode, regA, regB, destReg = "done", item[0], None, None, None
+                if (item[0]  == "halt"):            #and spacial case for "halt" it can be in both item[0] and item[1] 
+                    labels, instcode, regA, regB, destReg = None, item[0], None, None, None
                 elif item[1] == "halt":
-                    labels, instcode, regA, regB, destReg = "done", item[1], None, None, None
+                    labels, instcode, regA, regB, destReg = item[0], item[1], None, None, None
                 else:
                     labels, instcode, regA, regB, destReg = None, item[0], item[1], item[2], item[3]        #if have the same name as the instruction in the instruction list
             elif (item[1] == ".fill") :
@@ -128,24 +152,21 @@ class AssemblyTranslator:
         splited = [i.split() for i in splited]          #split each element in arr to sub list in from [['asdasd', 'add', '1', '2', '3'], ...]
         print(splited)                                  #!for debugging purposes
 
-        instList = self.__simplify(splited)               #contains all assembly code in instruction list format like [labels, instcode, regA, regB, destReg]
+        instList = self.__simplify(splited)             #contains all assembly code in instruction list format like [labels, instcode, regA, regB, destReg]
         self.__assembly= instList
-        print(*instList,sep='\n')                                 #!for debugging purposes
+        print(*instList,sep='\n')                       #!for debugging purposes
         
-        self.__fillFinding()
-        print(self.__fillValue)
-        self.translator([None, 'add', '7', '5', '7'])
+        self.__fillFinding()                            #!for debugging purposes
+        print(self.__fillValue)                         #!for debugging purposes
+        self.translator([None, 'lw', '0', '2', 'neg1'])   #!for debugging purposes
 
-        return instList
 
-        
-        
 
 
 if __name__ == "__main__":
+
     asbt = AssemblyTranslator()
+
     asbt.stringReader()
 
-    
-    
     #then read instList and decode them as binary string
