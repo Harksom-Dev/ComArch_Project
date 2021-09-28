32724db0045bd22db55162fc36946cf54318cb01
PossiblePack <possible.pack@gmail.com>
2021-09-29 02:17:49 +0700

    SimulationWithComment

--- RegWriteback.py
+++ RegWriteback.py
@@ -2,74 +2,43 @@ from os import stat
 from Convert import ConB
 from Main import *
 
-def printStruct(x):
+def printStruct(x): #use for print in each step
         print('@@@\nstate:')
         print('\tpc :',state.pc+1)
         print('\tmemory:')
-        for i in range(len(DEFMEMORY)):
+        for i in range(len(DEFMEMORY)):     #print each memory in that step
             print('\t\tmem[ {} ] {}'.format(i,int(DEFMEMORY[i])))
         print('\tregisters:')
-        for i in range(len(DEFREGS)):
+        for i in range(len(DEFREGS)):   #print each registers in that step
             print('\t\treg[ {} ] {}'.format(i,state.reg[i]))
         print('end state')
         return ''
 
 def Simulate():
-    Programcount=0
-    state.pc = -1
+    instructionCount=0  #for count Instruction 
+    state.pc = -1   #state.mem(0) for Initial registers
     while(state.pc != state.numMemory):
         a = ConB(int(state.mem[state.pc]))
         a.findReg()
         test = compute(int(a.opcode),int(a.regA),int(a.regB),int(a.regC))
-        if(test == 'noop'):
-            print(printStruct(state.pc))
+        if(test == 'noop'): #if compute return noop
+            print(printStruct(state.pc))    
             state.pc+=1
-            Programcount+=1
-        elif(test == 'halt'):
-            print('machine halted \n total of {} instructions executed\n final state of machine:'.format(Programcount))
+            instructionCount+=1
+        elif(test == 'halt'):   #if compute return halt
+            print('machine halted \n total of {} instructions executed\n final state of machine:'.format(instructionCount))
             print(printStruct(state.pc))
             state.pc += 1
-            Programcount+=1
+            instructionCount+=1
             break
-        elif(test == 'notjump'):
+        elif(test == 'notjump'):    #if compute return notjump
             print(printStruct(state.pc))
             state.pc += 1
-            Programcount+=1
-        else:
+            instructionCount+=1
+        else:   #if compute return Jump Address
             state.pc +=  test +1
             print(printStruct(state.pc))
-            Programcount+=1
-            #address = state.pc
-        #print("#############################################################",state.pc)
+            instructionCount+=1
         
-
-        
+ 
 Simulate()
-#print(DEFREGS)
-
-
-
-
-
-    # #=================================CHECK================================================#
-    # print('Mem is ',format(i))
-    # print(int(state.mem[i]))
-    # print('opcode is {}'.format(a.opcode))
-    # print('regA is {}'.format(a.regA))
-    # print('regB is {}'.format(a.regB))
-    # print('regC is {}'.format(a.regC))
-    # print('return is ',format(compute(int(a.opcode),int(a.regA),int(a.regB),int(a.regC))))
-    # print('_________________________________________________')
-    # #=================================CHECK================================================#
-
-
-# a = ConB(int(state.mem[0]))
-# a.findReg()
-# # print('opcode is {}'.format(a.opcode))
-# # print('regA is {}'.format(a.regA))
-# # print('regB is {}'.format(a.regB))
-# # print('regC is {}'.format(a.regC))
-# print(DEFMEMORY)
-# print(DEFREGS)
-# print(compute(int(a.opcode),int(a.regA),int(a.regB),int(a.regC)))
-# print('_________________________________________________')
