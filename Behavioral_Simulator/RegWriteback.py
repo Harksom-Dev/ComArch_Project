08a23ba8a293069ba3031f30d714de36488691d2
PossiblePack <possible.pack@gmail.com>
2021-10-03 21:33:10 +0700

    CleanCode&Comment

--- Behavioral_Simulator/RegWriteback.py
+++ Behavioral_Simulator/RegWriteback.py
@@ -16,44 +16,33 @@ def printStruct(x): #use for print in each step
     for i in range(len(DEFMEMORY)):     #print each memory in that step
         textList.append('\t\t\tmem[ {} ] {}'.format(i,int(DEFMEMORY[i])))
     textList.append('\tregisters:')
-    for i in range(len(DEFREGS)):   #print each registers in that step
+    for i in range(len(DEFREGS)):       #print each registers in that step
         textList.append('\t\t\treg[ {} ] {}'.format(i,state.reg[i]))
     textList.append('end state\n')
     return ''
 
 def Simulate():
-    SimulateEX = []
     instructionCount = 0  #for count Instruction 
-    #state.pc = -1   #state.mem(0) for Initial registers
     while(state.pc != state.numMemory):
-        a = ConB(int(state.mem[state.pc]))
-        a.findReg()
-        printStruct(state.pc)
-        test = compute(int(a.opcode),int(a.regA),int(a.regB),int(a.regC))
+        DATA = ConB(int(state.mem[state.pc]))   #store state of memory to DATA variable
+        DATA.findReg()                          #for defined register
+        printStruct(state.pc)                   #call function printState for write file
+        test = compute(int(DATA.opcode),int(DATA.regA),int(DATA.regB),int(DATA.regC))   #use for check instruction
         instructionCount+=1
-        if(test == 'noop'): #if compute return noop
-            #print(printStruct(state.pc))    
+        if(test == 'noop'):         #if compute return noop  
             state.pc+=1
-            #instructionCount+=1
-        elif(test == 'halt'):   #if compute return halt
+        elif(test == 'halt'):       #if compute return halt
             textList.append('machine halted \n total of {} instructions executed\n final state of machine:\n'.format(instructionCount))
-            #print(printStruct(state.pc))
             state.pc += 1
-            #instructionCount+=1
             break
         elif(test == 'notjump'):    #if compute return notjump
-            #print(printStruct(state.pc))
             state.pc += 1
-            #instructionCount+=1
-        elif(test == 'jalr'):
+        elif(test == 'jalr'):       #if compute return jalr
             state.pc += 0
-        else:   #if compute return Jump Address
+        else:                       #if compute return Jump Address
             state.pc +=  test +1
-            #print(printStruct(state.pc))
-            #instructionCount+=1
     
-
-    textList.append(printStruct(state.pc))
+    textList.append(printStruct(state.pc))  #append the result to the file 
  
-Simulate()
+Simulate()                                  #Call function for simulate assambly translator
 printer("ExSimulator.txt",textList)         #Write file from textList(Simulater list) to ExSimulator.txt file
