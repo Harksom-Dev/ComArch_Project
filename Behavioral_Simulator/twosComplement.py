1f7b768fad954522b8e7dab67b6156ddc22d4eda
phird <59276805+phird@users.noreply.github.com>
2021-10-03 21:52:30 +0700

    Update twosComplement.py

--- Behavioral_Simulator/twosComplement.py
+++ Behavioral_Simulator/twosComplement.py
@@ -1,7 +1,7 @@
 class twosComplement:
     #instruction    decBin | dec(int) , bit(int)
     #               binDec | num(string), bit(int)
-    def twosCom_decBin(dec, bit):
+    def twosCom_decBin(dec, bit):   #this convert decimal to binary in 2's Complemnt
         if dec >= 0 :
             bin1 = bin(dec).split("0b")[1]
             while len(bin1) < bit:
@@ -11,7 +11,7 @@ class twosComplement:
             bin1 = -1*dec
             return bin(bin1 - pow(2,bit) ).split("0b")[1]
     
-    def twosCom_binDec(num,bit):
+    def twosCom_binDec(num,bit): #this convert Binary to Decimal in 2's Complemnt
         while len(num) < bit:
             num = '0' + num
         if num[0] == '0':
